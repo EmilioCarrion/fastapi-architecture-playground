4f9f36418c09f20e894aa8733cd53e723aa0d0c5
Emilio Carrión <emcaryp@gmail.com>
2022-11-30 11:42:59 +0100

    Improve criteria handling

--- orders/adapters/in_memory_order_repository.py
+++ orders/adapters/in_memory_order_repository.py
@@ -1,26 +1,42 @@
 from __future__ import annotations
 from orders.application.domain.ports.required.repositories import OrderRepository, Equals, Not, Criteria
 from orders.application.domain.models.order import Order
-from typing import Any, List
+from typing import Any, List, Type, Union
+
+
+def handle_equals(criteria: Equals, orders: List[Order]):
+    return list(filter(lambda x: getattr(x, criteria.field) == criteria.value, orders))
+
+
+def handle_not(criteria: Not, orders: List[Order]):
+    handler = CRITERIA_HANDLERS[criteria.criteria]
+    excluded = handler(criteria.criteria, orders)
+    return [order for order in orders if order not in excluded]
+
+
+CRITERIA_HANDLERS = {
+    Equals: handle_equals,
+    Not: handle_not
+}
+
+
+AcceptedCriteria = Union[Equals, Not]
 
 
 class InMemoryOrderRepository(OrderRepository):
     def __init__(self):
         self.orders = {}
 
-    def find(self, *criteria_list: Criteria):
+    def find(self, *criteria_list: AcceptedCriteria):
         orders = self.orders.values()
         for criteria in criteria_list:
             orders = self._apply_criteria(orders, criteria)
         return orders
 
-    def _apply_criteria(self, orders: List[Order], criteria: Criteria):
-        if isinstance(criteria, Equals):
-            return list(filter(lambda x: getattr(x, criteria.field) == criteria.value, orders))
-        if isinstance(criteria, Not):
-            excluded = self._apply_criteria(orders, criteria.criteria)
-            return [order for order in orders if order not in excluded]
-        return orders
+    @staticmethod
+    def _apply_criteria(orders: List[Order], criteria: AcceptedCriteria):
+        handler = CRITERIA_HANDLERS[type(criteria)]
+        return handler(criteria, orders)
 
     def create_order(self, order: Order):
         self.orders[order.id] = order
